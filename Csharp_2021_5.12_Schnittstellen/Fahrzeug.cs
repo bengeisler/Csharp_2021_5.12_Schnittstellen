bc538a8bebcfb34762aeaa7b94d3bbb5db55046b
Benedikt Geisler <benedikt.geisler@posteo.de>
2021-02-15 14:26:32 +0100

    Projektdateien hinzufügen.

---
+++ Csharp_2021_5.12_Schnittstellen/Fahrzeug.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Csharp_2021_5._12_Schnittstellen
+{
+	// Die Klasse Fahrzeug implementiert die Schnittstelle ICloneable
+	// => Die Klasse Fahrzeug MUSS die Methode Clone() enthalten
+	class Fahrzeug : ICloneable
+	{
+		private int geschwindigkeit;
+
+		public void Beschleunigen(int wert) => geschwindigkeit += wert;
+
+		// Clone() wird in der Schnittstelle ICloneable definiert
+		public object Clone()
+		{
+			// Gibt ein neues Fahrzeug zurück, das die 
+			// Eigenschaften des bestehenden Fahrzeugs besitzt
+
+			// Neues Fahrzeug erstellen
+			Fahrzeug neuesFahrzeug = new Fahrzeug();
+
+			// Alle Eigenschaften des aktuellen Fahrzeugs übernehmen
+			// this: optional
+			neuesFahrzeug.geschwindigkeit = this.geschwindigkeit;
+
+			// Neues Fahrzeug zurückgeben
+			return neuesFahrzeug;
+		}
+
+		public override string ToString()
+		{
+			return "Geschwindigkeit: " + geschwindigkeit + "\n";	
+		}
+	}
+}
---
+++ Csharp_2021_5.12_Schnittstellen/Form1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Csharp_2021_5._12_Schnittstellen
+{
+	public partial class Form1 : Form
+	{
+		public Form1()
+		{
+			InitializeComponent();
+		}
+
+		private void Form1_Load(object sender, EventArgs e)
+		{
+			// Ein Objekt vom Typ Fahrzeug erstellen
+			Fahrzeug meinFahrzeug = new Fahrzeug();
+			meinFahrzeug.Beschleunigen(10);
+			Console.WriteLine(meinFahrzeug);
+
+			// Objekt "meinFahrzeug" kopieren
+			// - Methode Clone() wird auf "meinFahrzeug" angewandt => "meinFahrzeug" wird kopiert
+			// - (Fahrzeug) : Das ist ein Cast (eine Umwandlung). Clone() liefert als Rückgabe
+			//                object => Das muss noch explizit in den Datentyp "Fahrzeug" umgewandelt 
+			//							  werden
+			// - Diese neu erzeugte Kopie wird der Variablen "meinZweitesFahrzeug" zugewiesen
+			Fahrzeug meinZweitesFahrzeug = (Fahrzeug)meinFahrzeug.Clone();
+
+			// Nach der Kopie
+			Console.WriteLine("Nach der Kopie:");
+			Console.WriteLine("Erstes Fahrzeug: " + meinFahrzeug);
+			Console.WriteLine("Kopiertes Fahrzeug: " + meinZweitesFahrzeug);
+
+			// Beide Fahrzeuge beschleunigen
+			meinFahrzeug.Beschleunigen(10);
+			meinZweitesFahrzeug.Beschleunigen(50);
+			Console.WriteLine("Nach dem Beschleunigen");
+			Console.WriteLine("Erstes Fahrzeug: " + meinFahrzeug);
+			Console.WriteLine("Kopiertes Fahrzeug: " + meinZweitesFahrzeug);
+		}
+	}
+}
